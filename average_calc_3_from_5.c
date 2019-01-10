5573d2aa9970d880f9f1fafe9dae6c255ccce61f
Бажен Левковець <bazhen.paseka@gmail.com>
2019-01-10 12:13:35 +0200

    ref

--- average_calc_3_from_5.c
+++ average_calc_3_from_5.c
@@ -110,7 +110,7 @@ uint32_t Calc_Average(uint32_t* value, uint8_t qnt)
 		HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
 	}
 
-	sprintf(DataChar," ; ");
+	sprintf(DataChar,"\r\n");
 	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
 
 	uint32_t rezult = 0;
